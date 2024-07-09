253b477ef35b5acac0a2a09e2b4b5aa518792774
Yoon <cse21jh@korea.ac.kr>
2024-07-10 00:02:21 +0900

    Edit input receiver
    보완해야 할 점 굉장히 많음... 임시 코드

--- Assets/Scripts/Core/InputReceiver.cs
+++ Assets/Scripts/Core/InputReceiver.cs
@@ -6,9 +6,9 @@ namespace Brain.Core
 {
     public class InputReceiver : MonoBehaviour
     {
-        private GameObject cubeToRotate = null;
+        //private GameObject cubeToRotate = null;
 
-        public void Init(GameObject cube)
+        /*public void Init(GameObject cube)
         {
             cubeToRotate = cube;
         }
@@ -16,11 +16,34 @@ namespace Brain.Core
         public void Clear()
         {
             cubeToRotate = null;
-        }
+        }*/
 
         private void Update()
         {
             // 입력에 맞는 동작
+            switch (Input.inputString) //카메라 위치에 따라 축 방향 수정 필요, CAPS LOCK을 켜지 않으면 인식 불가
+            {
+                case "A":
+                    transform.Rotate(new Vector3(-90, 0, 0));
+                    break;
+                case "D":
+                    transform.Rotate(new Vector3(90, 0, 0));
+                    break;
+                case "S":
+                    transform.Rotate(new Vector3(0, -90, 0));
+                    break;
+                case "W":
+                    transform.Rotate(new Vector3(0, 90, 0));
+                    break;
+                case "Q":
+                    transform.Rotate(new Vector3(0, 0, -90));
+                    break;
+                case "E":
+                    transform.Rotate(new Vector3(0, 0, 90));
+                    break;
+
+            }
+
         }
     }
 }
